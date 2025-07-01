78f0fd9d1e9318aea14cabc7d7194cd60de7dd05
monickecunha <monicke.oliveira@hotmail.com>
2025-07-01 15:27:49 -0300

    Primeiro commit - Desafio Controle de Fluxo

---
+++ src/com/desafio/controlefluxo/exception/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package com.desafio.controlefluxo.exception;
+
+public class ParametrosInvalidosException extends RuntimeException{
+	public ParametrosInvalidosException(String message) {
+		super (message);
+	}
+}
